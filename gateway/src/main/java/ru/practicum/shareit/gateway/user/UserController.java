2ef8e746f8231144731354a2007f01c22b503ee0
AndrewColonel <andrewcolonel@yandex.ru>
2025-07-12 12:27:49 +0300

    Спринт 16 - ShareIt - add-item-requests-and-gateway - controllers - #8

--- gateway/src/main/java/ru/practicum/shareit/gateway/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/user/UserController.java
@@ -1,20 +1,27 @@
 package ru.practicum.shareit.gateway.user;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@AllArgsConstructor
-@RequestMapping("/users")
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
 public class UserController {
 
     private final UserClient userClient;
 
-
     // GetMapping  public Collection<UserDto> getAll()
-
+    @GetMapping
+    public ResponseEntity<Object> getUsers() {
+        return null;
+    }
 
     // GetMapping("/{id}") public UserDto getById(@Positive  @PathVariable("id") long id)
 
